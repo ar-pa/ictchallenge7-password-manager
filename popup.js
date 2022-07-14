684f48e9dc0416716a3fe2b5d0df6543f0d855fd
pourakhavan <pourakhavan@mahsan.co>
2022-07-15 02:44:24 +0430

    init

--- popup.js
+++ popup.js
@@ -1,26 +1,20 @@
-async function test() {
-    let table = document.getElementById("passwords");
-    var myHeaders = new Headers();
+async function create_table() {
+    const table = document.getElementById("passwords");
+    let r;
+    try {
+        r = await getPasswords()
+    } catch (e) {
+        console.log(e);
+        if (e === "Login First")
+            table.insertRow().insertCell().innerHTML = "Something goes wrong. Did you login?";
+    }
 
-
-    myHeaders.append("X-API-KEY", (await chrome.storage.sync.get('apiKey')).apiKey);
-
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-    let url = (await chrome.storage.sync.get('publicURL')).publicURL +
-        (await chrome.storage.sync.get('username')).username + '.json';
-    let r = await (await fetch(url, requestOptions)).json();
-
-    for (let x in r)
-        if (x !== 'master_password') {
-            const row = table.insertRow()
-            row.insertCell().innerHTML = x
-            row.insertCell().innerHTML = r[x][0]
-            row.insertCell().innerHTML = r[x][1]
-        }
+    for (const x in r) {
+        const row = table.insertRow()
+        row.insertCell().innerHTML = x
+        row.insertCell().innerHTML = r[x][0]
+        row.insertCell().innerHTML = r[x][1]
+    }
 }
 
-test()
+create_table()
---
+++ routines.js
@@ -0,0 +1,64 @@
+async function updatePasswords(passwords_json) {
+    let username;
+    let password;
+    try {
+        username = (await chrome.storage.sync.get('username')).username
+        password = (await chrome.storage.sync.get('password')).password
+    } catch {
+        throw "Login first";
+    }
+
+    console.log(passwords_json)
+
+    passwords_json['master_password'] = CryptoJS.SHA256(password).toString();
+    for (let x in passwords_json)
+        passwords_json[x][1] = CryptoJS.AES.encrypt(passwords_json[x][1], password).toString();
+
+    const myHeaders = new Headers();
+    myHeaders.append("X-API-KEY", (await chrome.storage.sync.get('apiKey')).apiKey);
+    myHeaders.append("Content-Type", "application/json");
+
+    const requestOptions = {
+        method: 'PUT',
+        headers: myHeaders,
+        body: JSON.stringify(passwords_json),
+        redirect: 'follow'
+    };
+    const url = (await chrome.storage.sync.get('publicURL')).publicURL + username + '.json';
+    await fetch(url, requestOptions);
+}
+
+async function getPasswords() {
+    let username, password;
+    try {
+        username = (await chrome.storage.sync.get('username')).username;
+        password = (await chrome.storage.sync.get('password')).password;
+    } catch {
+        throw "Login first";
+    }
+
+    const myHeaders = new Headers();
+    myHeaders.append("X-API-KEY", (await chrome.storage.sync.get('apiKey')).apiKey);
+
+    const requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+    };
+
+    let r;
+    try {
+        const url = (await chrome.storage.sync.get('publicURL')).publicURL + username + '.json';
+        r = await (await fetch(url, requestOptions)).json();
+    } catch {
+        throw "No such user";
+    }
+
+    if (r.master_password === CryptoJS.SHA256(password).toString()) {
+        delete r['master_password'];
+        for (let x in r)
+            r[x][1] = CryptoJS.AES.decrypt(r[x][1], password).toString(CryptoJS.enc.Utf8);
+        return r;
+    } else
+        throw "Wrong password";
+}
